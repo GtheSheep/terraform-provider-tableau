5e64620d041b5e4a5a4d007982e92ba01f787e8a
Juha Ylitalo <juha@ylitalot.net>
2025-03-13 13:13:45 +0200

    fix - log.Fatal(err) if terraform returns error (#51)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/gthesheep/terraform-provider-tableau/tableau"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -11,7 +12,10 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name tableau
 
 func main() {
-	providerserver.Serve(context.Background(), tableau.New, providerserver.ServeOpts{
+	err := providerserver.Serve(context.Background(), tableau.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/gthesheep/tableau",
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
