7ea4098afb8bae30f6b885995bbc98f583bee5b1
Juha Ylitalo <juha@ylitalot.net>
2025-03-25 01:07:38 +0200

    feature - Adds tableau_virtual_connection as data source (#49)
    * Adds tableau_virtual_connection as data source
    
    * More sensible names for virtual connection structs
    
    * Merge VirtualConnection and ListedVirtualConnection into single struct
    
    * Escaping has already been removed from Content's JSON
    
    * Update data-sources/virtual_connection.md
    
    ---------
    
    Co-authored-by: Gary James <13685708+GtheSheep@users.noreply.github.com>

--- tableau/virtual_connection.go
+++ tableau/virtual_connection.go
@@ -7,7 +7,14 @@ import (
 )
 
 type VirtualConnection struct {
-	ID          string `json:"id,omitempty"`
+	ID      string `json:"id,omitempty"`
+	Project struct {
+		ID string `json:"id,omitempty"`
+	} `json:"project,omitempty"`
+	Owner struct {
+		ID string `json:"id,omitempty"`
+	} `json:"owner,omitempty"`
+	Content     string `json:"content,omitempty"`
 	Name        string `json:"name,omitempty"`
 	CreatedAt   string `json:"createdAt,omitempty"`
 	UpdatedAt   string `json:"updatedAt,omitempty"`
@@ -20,6 +27,10 @@ type VirtualConnectionsRequest struct {
 	VirtualConnection VirtualConnection `json:"virtualConnection"`
 }
 
+type VirtualConnectionResponse struct {
+	VirtualConnection VirtualConnection `json:"virtualConnection"`
+}
+
 type VirtualConnectionsResponse struct {
 	VirtualConnections []VirtualConnection `json:"virtualConnection"`
 }
@@ -27,6 +38,25 @@ type VirtualConnectionsResponse struct {
 type VirtualConnectionsListResponse struct {
 	VirtualConnectionsResponse VirtualConnectionsResponse `json:"virtualConnections"`
 	Pagination                 PaginationDetails          `json:"pagination"`
+}
+
+func (c *Client) GetVirtualConnection(ID string) (*VirtualConnection, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/virtualconnections/%s", c.ApiUrl, ID), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	virtualConnectionResponse := VirtualConnectionResponse{}
+	err = json.Unmarshal(body, &virtualConnectionResponse)
+	if err != nil {
+		return nil, err
+	}
+	return &virtualConnectionResponse.VirtualConnection, nil
 }
 
 func (c *Client) GetVirtualConnections() ([]VirtualConnection, error) {
